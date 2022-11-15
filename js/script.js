cfbed507f7cedf55a807030a22d1bb3f8648d7ac
Viktar <viktar13@hotmail.com>
2022-11-15 17:21:43 +0400

    061 Added First Slider

--- js/script.js
+++ js/script.js
@@ -200,42 +200,42 @@ window.addEventListener("DOMContentLoaded", () => {
   // const div = new MenuCard();
   // div.render();
 
-  getResource("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
-      new MenuCard(
-        img,
-        altimg,
-        title,
-        descr,
-        price,
-        ".menu .container"
-      ).render();
-    });
-  });
-
-  // getResource("http://localhost:3000/menu")
-  //  .then(data => createCard(data));
-
-  // function createCard(data) {
+  // getResource("http://localhost:3000/menu").then((data) => {
   //   data.forEach(({ img, altimg, title, descr, price }) => {
-  //     const elem = document.createElement('div');
-
-  //     elem.classList.add('menu__item');
-
-  //     elem.innerHTML = `
-  //       <img src=${img} alt=${altimg} />
-  //       <h3 class="menu__item-subtitle">${title}</h3>
-  //       <div class="menu__item-descr">${descr}</div>
-  //       <div class="menu__item-divider"></div>
-  //       <div class="menu__item-price">
-  //         <div class="menu__item-cost">Цена:</div>
-  //         <div class="menu__item-total"><span>${price}</span> тал/день</div>
-  //       </div>
-  //     `;
-
-  //     document.querySelector(".menu .container").append(elem); 
+  //     new MenuCard(
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price,
+  //       ".menu .container"
+  //     ).render();
   //   });
-  // }
+  // });
+
+  getResource("http://localhost:3000/menu")
+   .then(data => createCard(data));
+
+  function createCard(data) {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      const elem = document.createElement('div');
+
+      elem.classList.add('menu__item');
+
+      elem.innerHTML = `
+        <img src=${img} alt=${altimg} />
+        <h3 class="menu__item-subtitle">${title}</h3>
+        <div class="menu__item-descr">${descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${price}</span> тал/день</div>
+        </div>
+      `;
+
+      document.querySelector(".menu .container").append(elem); 
+    });
+  }
 
 
 
@@ -374,6 +374,58 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // 058
 
-  fetch("http://localhost:3000/menu").then((data) => data.json());
+  // fetch("http://localhost:3000/menu").then((data) => data.json());
   // .then(result => console.log(result));
+
+  //061 Slider
+
+  const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex); 
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => item.style.display = 'none');
+
+    slides[slideIndex - 1].style.display = 'block';
+
+   if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
+
+
 });
