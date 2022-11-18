68326b01ef3c7ccb20e412d3061917e165c3128c
Viktar <viktar13@hotmail.com>
2022-11-18 16:37:42 +0400

    065 addea RegExs

--- js/script.js
+++ js/script.js
@@ -468,11 +468,17 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function onlyDigits(str) {
+    return +str.replace(/\D/g, "");
+  }
+
+
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    // if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == onlyDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += onlyDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -499,9 +505,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0 ) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = onlyDigits(width)* (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= onlyDigits(width);
     }
 
 
@@ -531,7 +537,7 @@ window.addEventListener("DOMContentLoaded", () => {
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = onlyDigits(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
